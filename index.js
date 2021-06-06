a3ca2f989f04cab127b28fbb532df0619cd3f371
Alivia Carruth <aliviarochester@gmail.com>
2021-06-06 17:21:12 -0400

    Additional setup files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const SlackBot = require('slackbots');
-const axios = require('axios');
-const cron = require('node-cron');
+// const axios = require('axios');
+// const cron = require('node-cron');
 
 dotenv.config();
 
@@ -10,37 +10,36 @@ const bot = new SlackBot({
     name: 'testing',
 });
 
-// bot.on('start', () => {
-//     const params = {
-//         icon_emoji: ':robot_face:'
-//     }
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
 
-//     bot.postMessageToChannel(
-//         'random',
-//         'Get inspired while working with @testing',
-//         params
-//     );
-	
-// });
-
-// bot.on('error', (err) => {
-//     console.log(err);
-// });
-
-function createJob() {
-    const task = cron.schedule(
-        '31 16 * * 1-5',
-        () => {
-            bot.postMessageToChannel('random', 'go eat lunch Alivia!');
-        },
-        {
-            scheduled: true,
-            timezone: 'America/New_York',
-        }
+    bot.postMessageToChannel(
+        'random',
+        'Get inspired while working with @inspirenuggets',
+        params
     );
+})
 
-    task.start();
-}
+bot.on('error', (err) => {
+    console.log(err);
+});
+
+// function createJob() {
+//     const task = cron.schedule(
+//         '31 16 * * 1-5',
+//         () => {
+//             bot.postMessageToChannel('random', 'go eat lunch Alivia!');
+//         },
+//         {
+//             scheduled: true,
+//             timezone: 'America/New_York',
+//         }
+//     );
+
+//     task.start();
+// }
 
 // bot.on('start', function () {
 //     bot.postMessageToUser('aliviarochester@gmail.com', 'testing app starting...');
